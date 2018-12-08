c59ea18aa9e52d8a78c9367dc89f3c0437469e60
Jeff Ducharme <jeff.m.ducharme@gmail.com>
2018-12-08 14:46:45 -0500

    UI updated
    Theme support added (light, dark, Solarized)
    Added the operations coolant type for V9 coolant
    Added the operations coolant state for X coolant

--- QuickParameters/ViewModel/MainViewViewModel.cs
+++ QuickParameters/ViewModel/MainViewViewModel.cs
@@ -10,22 +10,40 @@
 
 namespace QuickParameters.ViewModel
 {
-    using QuickParameters.Commands;
-
+    using System;
+    using System.Windows.Media;
     using System.ComponentModel;
     using System.Runtime.CompilerServices;
     using System.Windows;
     using System.Windows.Input;
 
     using Mastercam.Database;
+    using Mastercam.Database.Types;
+    using Mastercam.Operations.Types;
 
     using GalaSoft.MvvmLight.Messaging;
-    using Mastercam.Database.Types;
-    using System;
+
+    using QuickParameters.Commands;
 
     public class MainViewViewModel : INotifyPropertyChanged
     {
         #region Private Fields
+
+        private SolidColorBrush backgroundBrush;
+
+        private Color backgroundColor;
+
+        private SolidColorBrush headingBrush;
+
+        private Color headingColor;
+
+        private SolidColorBrush labelBrush;
+
+        private Color labelColor;
+
+        private SolidColorBrush parameterBrush;
+
+        private Color parameterColor;
 
         private string operationName;
 
@@ -39,7 +57,11 @@ namespace QuickParameters.ViewModel
 
         private int spindleSpeed;
 
+        private string coolant;
+
         private string toolPlane;
+
+        private string workOffset;
 
         #endregion
 
@@ -49,6 +71,11 @@ namespace QuickParameters.ViewModel
         {
             Messenger.Default.Register<Operation>(this, o => ReceiveSelectedOperation(o));
 
+            this.OnLightThemeCommand(null);
+
+            this.LightThemeCommand = new DelegateCommand(this.OnLightThemeCommand);
+            this.DarkThemeCommand = new DelegateCommand(this.OnDarkThemeCommand);
+            this.SolarizedThemeCommand = new DelegateCommand(this.OnSolarizedThemeCommand);
             this.CloseCommand = new DelegateCommand(this.OnCloseCommand);
         }
 
@@ -56,11 +83,109 @@ namespace QuickParameters.ViewModel
 
         #region Commands
 
+        public ICommand LightThemeCommand { get; }
+
+        public ICommand DarkThemeCommand { get; }
+
+        public ICommand SolarizedThemeCommand { get; }
+
         public ICommand CloseCommand { get; }
 
         #endregion
 
         #region Public Properties
+
+        public SolidColorBrush BackgroundBrush
+        {
+            get => this.backgroundBrush;
+
+            set
+            {
+                this.backgroundBrush = value;
+                OnPropertyChanged(nameof(this.BackgroundBrush));
+            }
+        }
+
+        public Color BackgroundColor
+        {
+            get => this.backgroundColor;
+
+            set
+            {
+                this.backgroundColor = value;
+                this.BackgroundBrush = new SolidColorBrush(this.backgroundColor);
+                OnPropertyChanged(nameof(this.BackgroundColor));
+            }
+        }
+
+        public SolidColorBrush HeadingBrush
+        {
+            get => this.headingBrush;
+
+            set
+            {
+                this.headingBrush = value;
+                OnPropertyChanged(nameof(this.HeadingBrush));
+            }
+        }
+
+        public Color HeadingColor
+        {
+            get => this.headingColor;
+
+            set
+            {
+                this.headingColor = value;
+                this.HeadingBrush = new SolidColorBrush(this.headingColor);
+                OnPropertyChanged(nameof(this.HeadingColor));
+            }
+        }
+
+        public SolidColorBrush LabelBrush
+        {
+            get => this.labelBrush;
+
+            set
+            {
+                this.labelBrush = value;
+                OnPropertyChanged(nameof(this.LabelBrush));
+            }
+        }
+
+        public Color LabelColor
+        {
+            get => this.labelColor;
+
+            set
+            {
+                this.labelColor = value;
+                this.LabelBrush = new SolidColorBrush(this.labelColor);
+                OnPropertyChanged(nameof(this.LabelColor));
+            }
+        }
+
+        public SolidColorBrush ParameterBrush
+        {
+            get => this.parameterBrush;
+
+            set
+            {
+                this.parameterBrush = value;
+                OnPropertyChanged(nameof(this.ParameterBrush));
+            }
+        }
+
+        public Color ParameterColor
+        {
+            get => this.parameterColor;
+
+            set
+            {
+                this.parameterColor = value;
+                this.ParameterBrush = new SolidColorBrush(this.parameterColor);
+                OnPropertyChanged(nameof(this.ParameterColor));
+            }
+        }
 
         public string OperationName
         {
@@ -128,6 +253,17 @@ namespace QuickParameters.ViewModel
             }
         }
 
+        public string Coolant
+        {
+            get => this.coolant;
+
+            set
+            {
+                this.coolant = value;
+                OnPropertyChanged(nameof(this.Coolant));
+            }
+        }
+
         public string ToolPlane
         {
             get => this.toolPlane;
@@ -139,20 +275,50 @@ namespace QuickParameters.ViewModel
             }
         }
 
+        public string WorkOffset
+        {
+            get => workOffset;
+
+            set
+            {
+                workOffset = value;
+                OnPropertyChanged(nameof(WorkOffset));
+            }
+        }
+
         #endregion
 
         #region Private Methods
 
         private object ReceiveSelectedOperation(Operation operation)
         {
-            this.OperationName = $"{GetOperationDescription(operation.Type)}{Environment.NewLine}" +
-                                 $"{operation.Name}";
-            this.ToolName = operation.OperationTool.Name;
-            this.ToolNumber = operation.OperationTool.Number;
-            this.ToolDiameter = operation.OperationTool.Diameter;
-            this.Feedrate = operation.FeedRate;
-            this.SpindleSpeed = operation.SpindleSpeed;
-            this.ToolPlane = operation.ToolPlane.ViewName;
+            if (operation != null)
+            {
+                this.OperationName = $"{GetOperationDescription(operation.Type)}{Environment.NewLine}" +
+                                     $"{operation.Name}";
+
+                if (operation.OperationTool != null)
+                {
+                    this.ToolName = operation.OperationTool.Name;
+                    this.ToolNumber = operation.OperationTool.Number;
+                    this.ToolDiameter = operation.OperationTool.Diameter;
+                }
+
+                this.Feedrate = operation.FeedRate;
+                this.SpindleSpeed = operation.SpindleSpeed;
+
+                if (operation.Coolant.OperationCoolant == 0)
+                {
+                    this.Coolant = GetCoolantStatus(operation.Coolant.States);
+                }
+                else
+                {
+                    this.Coolant = GetCoolantStatus(operation.Coolant.OperationCoolant);
+                }
+                
+                this.ToolPlane = operation.ToolPlane.ViewName;
+                this.WorkOffset = GetWorkOffset(operation.ToolPlane.WorkOffsetNumber);
+            }
 
             return null;
         }
@@ -163,7 +329,7 @@ namespace QuickParameters.ViewModel
             {
                 case 1:
                     return "Contour";
-                    
+
                 case 2:
                     return "Drilling";
 
@@ -274,6 +440,13 @@ namespace QuickParameters.ViewModel
                 case 134:
                     return "2D High Speed";
 
+                case 140:
+                    return "2D Model Chamfer";
+
+                case 154:
+                case 155:
+                    return "Linking";
+
                 case 443:
                     return "Multiaxis Parallel";
 
@@ -304,6 +477,104 @@ namespace QuickParameters.ViewModel
                 default:
                     return "Undefined";
             }
+        }
+
+        private string GetCoolantStatus(CoolantState states)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (states[i] == CoolantStateType.On)
+                {
+                    return "On";
+                }
+            }
+            return "Off";
+        }
+
+        private string GetCoolantStatus(CoolantMode mode)
+        {
+            switch (mode)
+            {
+                case CoolantMode.COOL_OFF:
+                    return "Off";
+                case CoolantMode.COOL_FLOOD:
+                    return "Flood";
+                case CoolantMode.COOL_MIST:
+                    return "Mist";
+                case CoolantMode.COOL_TOOL:
+                    return "Thru-Tool";
+                default:
+                    return "Unknown";
+            }
+        }
+
+        private string GetWorkOffset(short workOffset)
+        {
+            if (workOffset < 0)
+            {
+                return "Automatic";
+            }
+            else
+            {
+                return workOffset.ToString();
+            }
+        }
+
+        private void OnLightThemeCommand(object parameter)
+        {
+            var background = Color.FromRgb(255, 255, 255);
+            this.BackgroundBrush = new SolidColorBrush(background);
+            this.BackgroundColor = background;
+
+            var heading = Color.FromRgb(0, 0, 0);
+            this.HeadingBrush = new SolidColorBrush(heading);
+            this.HeadingColor = heading;
+
+            var labels = Color.FromRgb(0, 0, 0);
+            this.LabelBrush = new SolidColorBrush(labels);
+            this.LabelColor = labels;
+
+            var parameters = Color.FromRgb(0, 0, 0);
+            this.ParameterBrush = new SolidColorBrush(parameters);
+            this.ParameterColor = parameters;          
+        }
+
+        private void OnDarkThemeCommand(object parameter)
+        {
+            var background = Color.FromRgb(40, 40, 40);
+            this.BackgroundBrush = new SolidColorBrush(background);
+            this.BackgroundColor = background;
+
+            var heading = Color.FromRgb(104, 157, 106);
+            this.HeadingBrush = new SolidColorBrush(heading);
+            this.HeadingColor = heading;
+
+            var labels = Color.FromRgb(152, 151, 26);
+            this.LabelBrush = new SolidColorBrush(labels);
+            this.LabelColor = labels;
+
+            var parameters = Color.FromRgb(146, 131, 116);
+            this.ParameterBrush = new SolidColorBrush(parameters);
+            this.ParameterColor = parameters;
+        }
+
+        private void OnSolarizedThemeCommand(object parameter)
+        {
+            var background = Color.FromRgb(0, 43, 54);
+            this.BackgroundBrush = new SolidColorBrush(background);
+            this.BackgroundColor = background;
+
+            var heading = Color.FromRgb(65, 133, 153);
+            this.HeadingBrush = new SolidColorBrush(heading);
+            this.HeadingColor = heading;
+
+            var labels = Color.FromRgb(38, 139, 210);
+            this.LabelBrush = new SolidColorBrush(labels);
+            this.LabelColor = labels;
+
+            var parameters = Color.FromRgb(101, 123, 131);
+            this.ParameterBrush = new SolidColorBrush(parameters);
+            this.ParameterColor = parameters;
         }
 
         private void OnCloseCommand(object parameter)
